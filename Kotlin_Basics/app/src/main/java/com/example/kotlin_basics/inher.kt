a91aed464c481fb4e2f1980b35eb68d3debca125
Soumen <soumenpal3241@gmail.com>
2023-09-23 11:53:10 +0530

    Inheritance in kotlin

--- Kotlin_Basics/app/src/main/java/com/example/kotlin_basics/inher.kt
+++ Kotlin_Basics/app/src/main/java/com/example/kotlin_basics/inher.kt
@@ -2,8 +2,18 @@ package com.example.kotlin_basics
 
 import android.renderscript.ScriptGroup.Binding
 
-open class Car(val name:String ,val brand : String){
+interface Driveable{
+    val maxSpeed:Double
+    fun drive():String
+    fun brake(){
+        println("The driveable is braking")
+    }
+}
+
+open class Car(val name:String ,val brand : String):Driveable{
     open var range : Double = 0.0
+    override val maxSpeed: Double = 30.0
+
     open fun extendRange(amount:Double){
         if(amount > 0){
             range += amount
@@ -12,16 +22,22 @@ open class Car(val name:String ,val brand : String){
     open fun drive(distance : Double){
         println("Drove distance $distance")
     }
+
+    override fun drive(): String {
+        return "Hello"
+    }
 }
 
-class Electric(name: String, brand: String, batteryLife:Double ) : Car(name, brand){
+
+class Electric(name: String, brand: String, batteryLife:Double ) : Car(name, brand) {
 
     override var range = batteryLife * 6
+    var chargerType = "Type1"
     override fun drive(distance: Double) {
         println("Drove for $distance KM on elctricity")
     }
-    fun drive(){
-        println("Drove for $range KM on elctricity")
+    override fun drive():String{
+        return "Drove for $range KM on elctricity"
     }
 
 }
@@ -33,6 +49,9 @@ fun main(){
     audiA3.drive(200.0)
     teslaS.drive(200.0)
     teslaS.drive()
+    teslaS.brake()
+    println(teslaS.chargerType)
+
 
 
 }
