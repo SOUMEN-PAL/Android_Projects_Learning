3c4cd9ea85a2233527d6b7e52d5db6b00a4527da
Soumen <soumenpal3241@gmail.com>
2023-10-01 00:51:10 +0530

    Minute to age

--- Kotlin_Basics/app/src/main/java/com/example/kotlin_basics/nestedclass.kt
+++ Kotlin_Basics/app/src/main/java/com/example/kotlin_basics/nestedclass.kt
@@ -1,13 +1,35 @@
 package com.example.kotlin_basics
 
+import android.icu.number.IntegerWidth
+
 
 class outer(){
     val name:String = "SId"
-    class inner{
+    // nested class cant excess outer class methods but inner does //
+    inner class inner{
         val description : String = "HEllo its the inner class which in static"
+        fun foo(){
+            println("Hello ${name}")
+        }
     }
 
 }
+
+
+fun getNumber(s: String):Int {
+    return try{
+        Integer.parseInt(s)
+    }catch (e:ArithmeticException){
+        0
+    }
+}
+
 fun main(){
-    println(outer.inner().description)
+    println(outer().inner().description)
+    val v = outer().inner()
+    v.foo()
+    val str = getNumber("10")
+
+
+
 }
