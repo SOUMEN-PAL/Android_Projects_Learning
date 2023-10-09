e0270d31649841c3263d0ad85d919088d1520434
Soumen <soumenpal3241@gmail.com>
2023-10-09 22:58:25 +0530

    Added decimal support kinda?

--- myCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ myCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -3,15 +3,38 @@ package com.example.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput : TextView? = null
+    private var lastNumeric = false
+    private var lastDot = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInput = findViewById(R.id.Output)
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this , "Button pressed" , Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+
     }
+
+    fun onCLear(view: View){
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
 }
